49ea30bfc6cde54ffa8fa31731c6808125a9319e
Erik Öjebo <erik.ojebo@gmail.com>
2015-10-10 09:28:31 +0200

    Added powershell script for creating nuget packages

---
+++ publish/pack.ps1
@@ -0,0 +1,15 @@
+Write-Host "Compiling project..."
+
+$ProjectFilePath = "$PSScriptRoot\..\src\SeleniumShield\SeleniumShield.csproj"
+
+# Get the actual MSBuild path from the registry
+$MSBuildToolsPathRegistryValue = Get-ItemProperty `
+    -Path "HKLM:\SOFTWARE\Microsoft\MSBuild\ToolsVersions\4.0"  `
+    -Name "MsBuildToolsPath"
+$MSBuildDirectory = $MSBuildToolsPathRegistryValue.MSBuildToolsPath
+
+& "$MSBuildDirectory\msbuild.exe" $ProjectFilePath '/t:Clean;Rebuild' /p:Configuration=Release
+
+Write-Host "Packing nuget package..."
+
+nuget pack $ProjectFilePath -Prop Configuration=Release
--- src/SeleniumShield/Properties/AssemblyInfo.cs
+++ src/SeleniumShield/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SeleniumShield")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A runner framework for increasing the robustness of longer Selenium automation flows")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Sigma AB")]
+[assembly: AssemblyCompany("Erik Öjebo")]
 [assembly: AssemblyProduct("SeleniumShield")]
-[assembly: AssemblyCopyright("Copyright © Sigma AB 2015")]
+[assembly: AssemblyCopyright("Copyright © Erik Öjebo 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
